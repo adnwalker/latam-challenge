96116b1ec3019000fa5028e946fce6e341101bca
adnwalker <adnwalker.dscience@gmail.com>
2023-10-23 00:00:34 -0500

    ft-model: tests passed, model saved. SNR

--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,13 @@
 import pandas as pd
-
 from typing import Tuple, Union, List
+from datetime import datetime
+import numpy as np
+import joblib
+
+# ML Libraries
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.utils import resample
 
 class DelayModel:
 
@@ -9,11 +16,61 @@ class DelayModel:
     ):
         self._model = None # Model should be saved in this attribute.
 
+    def get_period_day(self, date):
+        date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+        morning_min = datetime.strptime("05:00", '%H:%M').time()
+        morning_max = datetime.strptime("11:59", '%H:%M').time()
+        afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+        afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+        evening_min = datetime.strptime("19:00", '%H:%M').time()
+        evening_max = datetime.strptime("23:59", '%H:%M').time()
+        night_min = datetime.strptime("00:00", '%H:%M').time()
+        night_max = datetime.strptime("4:59", '%H:%M').time()
+        
+        if(date_time > morning_min and date_time < morning_max):
+            return 'mañana'
+        elif(date_time > afternoon_min and date_time < afternoon_max):
+            return 'tarde'
+        elif(
+            (date_time > evening_min and date_time < evening_max) or
+            (date_time > night_min and date_time < night_max)
+        ):
+            return 'noche'
+
+    def is_high_season(self, fecha):
+        fecha_año = int(fecha.split('-')[0])
+        fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+        range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
+        range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
+        range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
+        range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
+        range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
+        range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
+        range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
+        range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
+        
+        if ((fecha >= range1_min and fecha <= range1_max) or 
+            (fecha >= range2_min and fecha <= range2_max) or 
+            (fecha >= range3_min and fecha <= range3_max) or
+            (fecha >= range4_min and fecha <= range4_max)):
+            return 1
+        else:
+            return 0
+        
+
+    def get_min_diff(self, data):
+        fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds())/60
+        return min_diff    
+    
+
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
+        
         """
         Prepare raw data for training or predict.
 
@@ -26,7 +83,48 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+
+        # Create a copy of the original data to avoid modifying the original DataFrame
+        #processed_data = data.copy()
+
+        # Apply 'get_period_day' function to create 'period_day' column
+        data['period_day'] = data['Fecha-I'].apply(self.get_period_day)
+
+        # Apply 'is_high_season' function to create 'high_season' column
+        data['high_season'] = data['Fecha-I'].apply(self.is_high_season)
+
+        # Apply 'get_min_diff' function to create 'min_diff' column
+        data['min_diff'] = data.apply(self.get_min_diff, axis=1)
+
+        # Apply 'delay' condition and create 'delay' column
+        threshold_in_minutes = 15
+        data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
+
+        features = pd.concat([
+            pd.get_dummies(data['OPERA'], prefix='OPERA'),
+            pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'),
+            pd.get_dummies(data['MES'], prefix='MES')
+        ], axis=1)
+
+        top_10_features = [
+            "OPERA_Latin American Wings", 
+            "MES_7",
+            "MES_10",
+            "OPERA_Grupo LATAM",
+            "MES_12",
+            "TIPOVUELO_I",
+            "MES_4",
+            "MES_11",
+            "OPERA_Sky Airline",
+            "OPERA_Copa Air"
+        ]
+
+        filtered_features = features[top_10_features]
+
+        if target_column:
+            return filtered_features, data[[target_column]]
+        else:
+            return filtered_features
 
     def fit(
         self,
@@ -40,7 +138,23 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        print("Fitting the model...")
+        
+        # Split data into training and testing sets
+        x_train, x_test, y_train, y_test = train_test_split(features, target['delay'], test_size=0.33, random_state=42)
+        # Balance Data
+        n_y0 = len(y_train[y_train == 0])
+        n_y1 = len(y_train[y_train == 1])
+        scale = n_y0 / n_y1
+
+        xgb_model = xgb.XGBClassifier(random_state=1, learning_rate=0.01, scale_pos_weight=scale)
+        xgb_model.fit(x_train, y_train)
+
+        # Store the trained model
+        self._model = xgb_model
+
+        # Save the model to a file
+        joblib.dump(self._model, 'xgboost_model.pkl')
 
     def predict(
         self,
@@ -55,4 +169,16 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+
+        # Check if the model has been trained
+        if self._model is None:
+            # Load the saved model from the file
+            self._model = joblib.load('xgboost_model.pkl')
+            
+            # if self._model is None:
+            #     raise ValueError("Model has not been trained. Call fit() first.")
+        
+        # Make predictions using the trained model
+        predictions = self._model.predict(features)
+        
+        return predictions.tolist()
